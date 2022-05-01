f9ed867b7cff17508ba70fac98fd3e7ff06fc2d3
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2022-05-01 10:33:04 -0700

    update unit tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 export class Queue {
-  constructor(_array = []) {
-    this._array = _array;
+  constructor(items = []) {
+    this.items = items;
   }
   enqueue(item) {
     let end = 0;
     if (item instanceof Array) {
       end = item.length;
-      this._array.unshift(...item);
+      this.items.unshift(...item);
     } else {
-      this._array.unshift(item);
+      this.items.unshift(item);
     }
-    return this._array.slice(0, end);
+    return this.items.slice(0, end);
   }
   dequeue() {
-    return this._array.shift();
+    return this.items.shift();
   }
   getItems() {
     return this.items;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,15 @@
 import { Queue } from "./index";
 
 test("creates a queue with one item", () => {
-  //   expect(getAboutUsLink("en-UK")).toBe("/about-us");
+  const queue = new Queue();
+  const item = 0;
+  queue.enqueue(item);
+  expect(queue.items).toEqual([item]);
+});
+test("creates a queue with multiple items", () => {
+  //   const queue = new Queue();
+  //   const items = [0, 1, 2, 3];
+  //   queue.enqueue(items);
+  //   console.log(queue);
+  //   expect(queue.items).toBe(items);
 });
